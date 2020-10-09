8a908452ba66f63f3746a378b35e91ebf025d4c6
Liam Bradley <liamhbradley11@gmail.com>
2020-10-08 21:18:46 -0400

    updated index with questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,32 +1,52 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writefile);
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [
+function promptUser(){
+  return inquirer.prompt([
     {
-        type: "input",
-        message: "What is your user name?",
-        name: "username"
-      },
-      {
-        type: "input",
-        message: "What is your password?",
-        name: "password"
-      },
-      {
-        type: "input",
-        message: "Re-enter password to confirm:",
-        name: "confirm"
-      }
-];
+      type: "input",
+      message: "What is your Git Hub username?",
+      name: "username"
+    },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "project"
+    },
+    {
+      type: "input",
+      message: "Provide a description of your project. What can it do?",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "What do you need to install the project?",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "project"
+    },
+    {
+      type: "input",
+      message: "What is the name of your project?",
+      name: "project"
+    },
+  ]
 
 
-inquirer
-  .prompt(questions)
+// inquirer
+//   .prompt(questions)
    
-  .then(function(response) {
-    console.log(response)
+//   .then(function(response) {
+//     console.log(response)
 
-    console.log(generateMarkdown(response))
+//     console.log(generateMarkdown(response))
 
     // if (response.confirm === response.password) {
     //   console.log("Success!");
